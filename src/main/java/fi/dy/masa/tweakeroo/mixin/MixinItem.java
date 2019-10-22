364e07e28ec57dd5cdcbb9a14a5d466c6e67c517
Salandora <salandora@gmail.com>
2019-10-22 21:47:53 +0200

    Cleanup

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinItem.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinItem.java
@@ -6,9 +6,6 @@ import fi.dy.masa.tweakeroo.util.IItemStackLimit;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.class)
 public abstract class MixinItem implements IItemStackLimit
